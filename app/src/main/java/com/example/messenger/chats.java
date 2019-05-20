15e0e3307fd7df27a56bdd7c64b86877040e084d
Mark Frelih <mark.frelih@gmail.com>
2019-05-20 19:05:30 +0200

    added top and bottom bar + dynamical element creation

--- app/src/main/java/com/example/messenger/chats.java
+++ app/src/main/java/com/example/messenger/chats.java
@@ -4,6 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 
 public class chats extends AppCompatActivity {
 
@@ -11,6 +14,7 @@ public class chats extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.chats);
+
     }
 
     public void openProfile(View v) {
@@ -19,5 +23,12 @@ public class chats extends AppCompatActivity {
         finish();
         startActivity(i);
 
+    }
+
+    public void newChat(View view) {
+        LinearLayout ll = findViewById(R.id.Chats);
+        Button btn = new Button(this);
+        btn.setText("test");
+        ll.addView(btn);
     }
 }
