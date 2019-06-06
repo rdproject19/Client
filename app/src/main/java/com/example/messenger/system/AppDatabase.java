9cddb3ba403c7004e3e6ed61ada775b73389347f
Thomaskolb <49525083+Thomaskolb@users.noreply.github.com>
2019-06-06 12:43:42 +0200

    Revert "Merge branch 'frontend' of https://github.com/rdproject19/Client into frontend"
    This reverts commit b50b498e2e3507e6274cc6ce6e2760aa9bbcac4c, reversing
    changes made to 77f28d4596d55512e5d7f6b574a44bcb478e7efc.

--- app/src/main/java/com/example/messenger/system/AppDatabase.java
+++ app/src/main/java/com/example/messenger/system/AppDatabase.java
@@ -6,7 +6,7 @@ package com.example.messenger.system;
 
 
 @Database(
-        entities = {Message.class, Conversation.class, Contact.class},
+        entities = {Message.class, Conversation.class},
         version = 1, exportSchema = false)
 
 public abstract class AppDatabase extends RoomDatabase
