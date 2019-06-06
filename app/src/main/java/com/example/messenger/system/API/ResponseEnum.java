24d51d022a5ef14311b1074a783c2adf53fc4f01
Karim Abdulahi <kj@abdulahi.nl>
2019-06-06 11:31:56 +0200

    l

--- app/src/main/java/com/example/messenger/system/API/ResponseEnum.java
+++ app/src/main/java/com/example/messenger/system/API/ResponseEnum.java
@@ -2,15 +2,22 @@ package com.example.messenger.system.API;
 
 public enum ResponseEnum {
 
-    OK(200),
+    OK(200, "Successfully executed request"),
     BAD_REQUEST(400),
     UNAUTHORIZED(401),
     GONE(410),
     NOT_FOUND(404),
     SERVER_ERROR(500),
-    IM_A_TEAPOT(418);
+    IM_A_TEAPOT(418, "If you see this, then probably something has logically gone wrong. Check exception trace. ");
 
     private int code;
+    private String desc;
+
+    ResponseEnum(int code, String desc)
+    {
+        this(code);
+        this.desc = desc;
+    }
 
     ResponseEnum(int code)
     {
@@ -21,6 +28,10 @@ public enum ResponseEnum {
         return code;
     }
 
+
+    public String getDesc() {
+        return this.desc;
+    }
 
 
 }
