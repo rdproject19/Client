90aa772b91a823b1ad0e030e061bd0e61fcceed9
Thomaskolb <49525083+Thomaskolb@users.noreply.github.com>
2019-06-07 19:54:21 +0200

    more intense colors in icon

--- app/src/main/java/com/example/messenger/system/AlertReceiver.java
+++ app/src/main/java/com/example/messenger/system/AlertReceiver.java
@@ -1,31 +1,17 @@
 package com.example.messenger.system;
 
-import android.app.Activity;
-import android.app.ActivityManager;
-import android.app.Notification;
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.BroadcastReceiver;
-import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.support.v4.app.NotificationCompat;
-import android.view.View;
 
 import com.example.messenger.ChatWindow;
 import com.example.messenger.Global;
 import com.example.messenger.R;
-
-import java.util.List;
-
-import static android.content.Context.NOTIFICATION_SERVICE;
-
 
 public class AlertReceiver extends BroadcastReceiver {
 
